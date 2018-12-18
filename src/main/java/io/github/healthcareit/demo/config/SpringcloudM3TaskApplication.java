200116160558e2f17ec08b4f3e5350d72c85b531
Healthcare IT <healthcare.it@outlook.com>
2018-12-18 13:33:15 -0500

    Initial commit

---
+++ src/main/java/io/github/healthcareit/demo/config/SpringcloudM3TaskApplication.java
@@ -0,0 +1,14 @@
+package io.github.healthcareit.demo.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudM3TaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudM3TaskApplication.class, args);
+	}
+
+}
+
